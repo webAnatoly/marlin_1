4b12b06ae9d3b4d1c07a4e0fc80e02731ba8765d
Tolik Dem <tolik.demianov@gmail.com>
2019-12-21 00:03:22 +0300

    Добавил в bash скрипт открытие вкладки с phpMyadmin

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker-compose up -d
 url="http://localhost:5555"
-#firefox -new-window --devtools $url &
+phpMyAdmin="http://localhost:5556"
+firefox -new-tab $phpMyAdmin &
+sleep 1
 firefox -new-tab $url &
 exit 0 #Выход с кодом 0 (удачное завершение работы скрипта)
