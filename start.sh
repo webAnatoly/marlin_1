62d2bfff8c8f7a6cdb5d518b8f3d1b1ff197a3d0
Tolik Dem <tolik.demianov@gmail.com>
2019-12-22 13:12:16 +0300

    Изменил команду запуска firefox в bash скрипте.

--- start.sh
+++ start.sh
@@ -2,7 +2,5 @@
 url="http://localhost:5555"
 phpMyAdmin="http://localhost:5556"
 docker-compose up -d
-firefox -new-tab $phpMyAdmin &
-sleep 1
-firefox -new-tab $url &
+firefox $url $phpMyAdmin
 exit 0 #Выход с кодом 0 (удачное завершение работы скрипта)
