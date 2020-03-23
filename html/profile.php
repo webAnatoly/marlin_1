91e9ef19720d3552386a1de682000c22e83a545f
Tolik Dem <tolik.demianov@gmail.com>
2020-03-23 11:56:04 +0300

    Начал делать профиль пользователя:
    - Добавил файл profile.php

---
+++ html/profile.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Comments</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="css/app.css" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/view/nav.php"?>
+
+    <main class="py-4">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header"><h3>Профиль пользователя</h3></div>
+
+                        <div class="card-body">
+                            <div class="alert alert-success" role="alert">
+                                Профиль успешно обновлен
+                            </div>
+
+                            <form action="" method="post" enctype="multipart/form-data">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label for="exampleFormControlInput1">Name</label>
+                                            <input type="text" class="form-control" name="name" id="exampleFormControlInput1" value="John">
+
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="exampleFormControlInput1">Email</label>
+                                            <input type="email" class="form-control is-invalid" name="email" id="exampleFormControlInput1" value="john@example.com">
+                                            <span class="text text-danger">
+                                                Ошибка валидации
+                                            </span>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="exampleFormControlInput1">Аватар</label>
+                                            <input type="file" class="form-control" name="image" id="exampleFormControlInput1">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <img src="img/no-user.jpg" alt="" class="img-fluid">
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <button class="btn btn-warning">Edit profile</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12" style="margin-top: 20px;">
+                    <div class="card">
+                        <div class="card-header"><h3>Безопасность</h3></div>
+
+                        <div class="card-body">
+                            <div class="alert alert-success" role="alert">
+                                Пароль успешно обновлен
+                            </div>
+
+                            <form action="/profile/password" method="post">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label for="exampleFormControlInput1">Current password</label>
+                                            <input type="password" name="current" class="form-control" id="exampleFormControlInput1">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="exampleFormControlInput1">New password</label>
+                                            <input type="password" name="password" class="form-control" id="exampleFormControlInput1">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="exampleFormControlInput1">Password confirmation</label>
+                                            <input type="password" name="password_confirmation" class="form-control" id="exampleFormControlInput1">
+                                        </div>
+
+                                        <button class="btn btn-success">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
+</body>
+</html>
