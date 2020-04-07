39597cf8854c554c8890a421857de0b15e7dfb1d
Tolik Dem <tolik.demianov@gmail.com>
2020-04-07 18:15:13 +0300

    Работа с полупрозрачными цветами

--- html/image.php
+++ html/image.php
@@ -4,3 +4,6 @@ var_dump(getimagesize($_SERVER["DOCUMENT_ROOT"] . "/uploads/avatar_8.png"));
 
 ?>
 <img src="View/button.php?Hello+world!">
+
+<h1>Работа с полупрозрачными цветами</h1>
+<img src="View/semitransp.php" alt="полупрозрачный рисунок">
