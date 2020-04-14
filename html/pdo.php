74a747a834c294d6559ea95314ee4bcd138fbe07
Tolik Dem <tolik.demianov@gmail.com>
2020-04-14 22:34:14 +0300

    WebPager - базовый начальный пример пагинации (постраничной навигации).
    - Добавил постраничную навигация по содержимому базы данных. В файле pdo.php выводятся записи из базы данных с построничной навигацией.
    
    - Это тренировочный код создания собственного компонента для размещения в репозитарии https://packagist.org чтобы потом его можно было устанавливать через Composer
    
    - В данном комите в файле pdo.php вариант с временным автозагрузчиком классов, просто чтобы проверить, что код работает.

---
+++ html/pdo.php
@@ -0,0 +1,53 @@
+<?php ## Постраничная навигация таблицы languages
+// Временная автозагрузка классов
+// Временная автозагрузка классов
+spl_autoload_register(
+    function ($class) {
+        // project-specific namespace prefix
+        $prefix = 'WebPager\\';
+
+        // base directory for the namespace prefix
+        $base_dir = $_SERVER['DOCUMENT_ROOT'].'/pager/src/';
+
+        // does the class use the namespace prefix?
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            // no, move to the next registered autoloader
+            return;
+        }
+
+        // get the relative class name
+        $relative_class = substr($class, $len);
+
+        // replace the namespace prefix with the base directory, replace namespace
+        // separators with directory separators in the relative class name, append
+        // with .php
+        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+        // if the file exists, require it
+        if (file_exists($file)) {
+            require $file;
+        }
+    }
+);
+try {
+    $pdo = new PDO(
+        'mysql:host=mysql_my_marlin_project_1;dbname=forum',
+        'root',
+        'test',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $obj = new WebPager\PdoPager(
+        new WebPager\PagesList(),
+        $pdo,
+        'languages');
+// Содержимое текущей страницы
+    foreach($obj->getItems() as $language) {
+        echo htmlspecialchars($language['name'])."<br /> ";
+    }
+// Постраничная навигация
+    echo "<p>$obj</p>";
+}
+catch (PDOException $e) {
+    var_dump($e->getMessage());
+    echo "Невозможно установить соединение с базой данных";
+}
